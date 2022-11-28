71914acf21ed349b14e13525c1fd265532551d83
Siska Mayra Andini <89381581+smandini@users.noreply.github.com>
2022-11-28 21:13:03 +0800

    Add files via upload

---
+++ about-me.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>About</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light justify-content-center text-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand fw-bold" href="#">iLectrical</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="about-me.php">About Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container p-5">
+        <div class="header text-center" style="color: #052c65; font-family: Broadway;">
+            <h1>About Me</h1>
+        </div><!-- end header-->
+        <div class="image text-center p-3">
+            <img src="woman.jpg" class="img-fluid rounded-circle" style="width:200px;" alt="Avatar">
+        </div><!-- end image-->
+        <div class="intro column">
+            </br>
+            <p>
+                I am Siska Mayra Andini; A four year college student in Electrical Engineering, Faculty of Engineering,
+                Mulawarman
+                University. Electrical engineering has two concentration includes a focus on Computer and Power
+                Engineering, now I am focusing on Computer Engineering.
+        </div><!-- end intro-->
+        <br>
+        <div class="row">
+            <div class="main-text text-center" style="color: #052c65;">
+                </br>
+                <h4>More About Me</h4>
+                </br>
+                <p>
+                    Born in the 21st century, perhaps I am a little like the grown-ups. I have had to grown-up.
+                    Read, write, read, write, run, and talk. I enjoy travelling and cycling.
+                    I watch more sunsets than netflix. The truth is I am a lover not a hater.
+            </div>
+        </div><!-- end main-text-->
+
+        <div class="row">
+            <div class="col">
+                <div class="header text-center p-5" style="color: #052c65; font-family: Broadway;">
+                    </br>
+                    <h1>About iLectrical</h1>
+                </div><!-- end header-->
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-8 position-relative">
+                <div class="position-absolute top-50 start-50 translate-middle">
+                </div>
+                <div class="row m-5">
+                    <h2>What is iLectrical ?</h2>
+                    <p>
+                        </br>
+                        iLectrical dedicates to my last exam project for Web Programming in my 7th semester.
+                        This project was created due to the idea to archive various book references obtained while
+                        studying at college. Because Electrical engineering at University of Mulawarman has two
+                        concentrations, all books here are devoted to computer engineering only.
+                    </P>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="cover">
+                    <br>
+                    <img src=" library.png" width="80%">
+                </div>
+            </div>
+
+        </div>
+        <!--end container-->
+
+        <div class="row">
+            <div class="col">
+                <div class="header text-center p-5" style="color: #052c65; font-family: Broadway;">
+                    </br>
+                    <h1>Our Faculty</h1>
+                </div><!-- end header-->
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="cover">
+                    <br>
+                    <img src="east.png" width="115%">
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="position-absolute top-50 start-50 translate-middle">
+                </div>
+                <div class="row m-5">
+                    <h2>Whare is Our Faculty ?</h2>
+                    <p>
+                        </br>
+                        Our faculty is part of Mulawarman University. The Universitas Mulawarman is a public university
+                        located in Samarinda, East Kalimantan, Indonesia. It was established on September 27, 1962,
+                        making it the oldest tertiary education institution in East Kalimantan.
+                        Faculty of Engineering at Mulawarman University located in Gunung Kalua and it has theme as
+                        'Tropical Engineering'.
+                </div>
+
+
+            </div>
+            <!--end container-->
+        </div>
+
+</body>
+
+</html>
